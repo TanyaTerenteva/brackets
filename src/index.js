46be11542a9541a95e4b31c6a073685ce9425969
Tatsiana Terenteva <tayeterry@gmail.com>
2023-01-27 23:47:16 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let resultStack = [];
+  let openElement = 0;
+
+  for (let index = 0; index < str.length; index++) {
+    for (const [open, close] of bracketsConfig) {
+      if (str[index] == close) {
+        if (resultStack[openElement - 1] == open) {
+          resultStack.pop();
+          openElement--;
+        } else {
+          resultStack.push(str[index]);
+          openElement++;
+        }
+      } else {
+        if (str[index] == open) {
+          resultStack.push(str[index]);
+          openElement++;
+        }
+      }
+    }
+  }
+
+  return !resultStack.length;
+};
